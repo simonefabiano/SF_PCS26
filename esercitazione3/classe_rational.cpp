a8767a78f81d3701dc14e417ada250d257403057
simonefabiano <fabiano.simone04@gmail.com>
2026-03-31 21:58:58 +0000

    consegna_es3

--- esercitazione3/classe_rational.cpp
+++ esercitazione3/classe_rational.cpp
@@ -2,14 +2,25 @@
 #include "classe_rational.hpp"
 
 int main() {
-    rational<int> r(3, 6);
-    std::cout << "Frazione semplificata: " << r << std::endl;
+    rational<int> r1(3, 6);
+    std::cout << "Semplificazione 3/6: " << r1 << "\n";
 
-    rational<int> a(1, 4);
+    rational<int> a(1, 2);
     rational<int> b(1, 4);
-    rational<int> c = a + b;
-    
-    std::cout << a << " + " << b << " = " << c << std::endl;
+
+
+    std::cout << a << " + " << b << " = " << (a + b) << "\n";
+    std::cout << a << " - " << b << " = " << (a - b) << "\n";
+    std::cout << a << " * " << b << " = " << (a * b) << "\n";
+    std::cout << a << " / " << b << " = " << (a / b) << "\n";
+ 
+    rational<int> inf(5, 0);
+    rational<int> nan(0, 0);
+    std::cout << "Test Inf: " << inf << "\n";
+    std::cout << "Test NaN: " << nan << "\n";
 
     return 0;
 }
+
+// IA impiegata per il tentativo di risoluzione (fallito) di problemi segnalati dal compiler che
+// impediscono di runnare il programma, e di cui si cercherà una risoluzione nelle vacanze di Pasqua
--- esercitazione3/classe_rational.hpp
+++ esercitazione3/classe_rational.hpp
@@ -1,3 +1,6 @@
+#ifndef CLASSE_RATIONAL_HPP
+#define CLASSE_RATIONAL_HPP
+
 #include <iostream>
 #include <concepts>
 #include <numeric>
@@ -9,42 +12,90 @@ private:
     I num_;
     I den_;
 
+    void simplify() {
+        if (den_ == 0) return; 
+        I common = std::gcd(num_, den_);
+        num_ /= common;
+        den_ /= common;
+        if (den_ < 0) { num_ = -num_; den_ = -den_; }
+    }
+
 public:
     rational() : num_(0), den_(1) {}
 
     rational(I n, I d) : num_(n), den_(d) {
-        if (den_ != 0) {
-            I comune = std::gcd(num_, den_);
-            num_ /= comune;
-            den_ /= comune;
-        }
+        simplify();
     }
 
     I num() const { return num_; }
     I den() const { return den_; }
-
+    
     rational& operator+=(const rational& other) {
         num_ = num_ * other.den_ + other.num_ * den_;
-        den_ = den_ * other.den_;
-        I comune = std::gcd(num_, den_);
-        num_ /= comune;
-        den_ /= comune;
+        den_ *= other.den_;
+        simplify();
+        return *this;
+    }
+
+    rational& operator-=(const rational& other) {
+        num_ = num_ * other.den_ - other.num_ * den_;
+        den_ *= other.den_;
+        simplify();
+        return *this;
+    }
+
+    rational& operator*=(const rational& other) {
+        num_ *= other.num_;
+        den_ *= other.den_;
+        simplify();
+        return *this;
+    }
+
+    rational& operator/=(const rational& other) {
+        num_ *= other.den_;
+        den_ *= other.num_;
+        simplify();
         return *this;
     }
 };
 
 template<typename I>
-rational<I> operator+(rational<I> a, const rational<I>& b) {
-    a += b;
-    return a;
+rational<I> operator+(rational<I> lhs, const rational<I>& rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+template<typename I>
+rational<I> operator-(rational<I> lhs, const rational<I>& rhs) {
+    lhs -= rhs;
+    return lhs;
+}
+
+template<typename I>
+rational<I> operator*(rational<I> lhs, const rational<I>& rhs) {
+    lhs *= rhs;
+    return lhs;
+}
+
+template<typename I>
+rational<I> operator/(rational<I> lhs, const rational<I>& rhs) {
+    lhs /= rhs;
+    return lhs;
 }
 
 template<typename I>
 std::ostream& operator<<(std::ostream& os, const rational<I>& r) {
     if (r.den() == 0) {
-        os << "NaN/Inf";
+        if (r.num() == 0) os << "NaN";
+        else os << "Inf";             
     } else {
-        os << r.num() << "/" << r.den();
+        os << r.num();
+        if (r.den() != 1) os << "/" << r.den();
     }
     return os;
 }
+
+#endif
+
+// IA impiegata per il tentativo di risoluzione (fallito) di problemi segnalati dal compiler che
+// impediscono di runnare il programma, e di cui si cercherà una risoluzione nelle vacanze di Pasqua
